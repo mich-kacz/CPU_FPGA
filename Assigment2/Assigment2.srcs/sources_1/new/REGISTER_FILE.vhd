940e9f8add77a20885ffe49b91d6ea7c8f039b34
Kaczmarczyk <mk23561@essex.ac.uk>
2024-02-20 17:21:59 +0000

    Created additional resources

---
+++ Assigment2/Assigment2.srcs/sources_1/new/REGISTER_FILE.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 20.02.2024 17:11:20
+-- Design Name: 
+-- Module Name: REGISTER_FILE - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity REGISTER_FILE is
+--  Port ( );
+end REGISTER_FILE;
+
+architecture Behavioral of REGISTER_FILE is
+
+begin
+
+
+end Behavioral;
---
+++ Assigment2/Assigment2.srcs/sources_1/new/ZERO_FLAG_REG.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 20.02.2024 17:06:07
+-- Design Name: 
+-- Module Name: ZERO_FLAG_REG - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ZERO_FLAG_REG is
+--  Port ( );
+end ZERO_FLAG_REG;
+
+architecture Behavioral of ZERO_FLAG_REG is
+
+begin
+
+
+end Behavioral;
